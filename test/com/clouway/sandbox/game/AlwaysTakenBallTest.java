389a95a2a12804d36c952a96103fb16ccd813023
Miroslav Genov <mgenov@gmail.com>
2016-06-01 18:45:57 +0300

    doc: improving comment of AlwaysTakenBallTest
    Comment of AlwaysTakenBallTest now contains information why this test is
    always failing.

--- test/com/clouway/sandbox/game/AlwaysTakenBallTest.java
+++ test/com/clouway/sandbox/game/AlwaysTakenBallTest.java
@@ -1,6 +1,8 @@
 package com.clouway.sandbox.game;
 
 /**
+ * AlwaysTakenBallTest will always fail, cause this ball is Always taken and {@link Player} is not able to play with it.
+ *
  * @author Miroslav Genov (miroslav.genov@clouway.com)
  */
 public class AlwaysTakenBallTest extends TakeAndReleaseBallTest {
